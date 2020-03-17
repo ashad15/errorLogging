852cd8c8c35035054eb8fe9588e131a1e1af89a4
ashad abbas <ashadabbas@ashads-MacBook-Air.local>
2020-03-17 17:33:38 +0530

    second

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
-
-
-console.log("in");
-//const app1= new App();
-
-
-(function() {
-  // Constructor function
-  function App (){
+const track =true;
+// Constructor function
+function App (){
     console.log("constructor");
     // Grab all events
+  if(track===true)
+  {
     window.addEventListener('error', e => {
-      // when an error occurs,
-      // send the error information
-      // to our yet to be created server
+    
+
+  
+
+
+      
+      console.log("hey its error");
+      console.log(e);
+     
       this.sendPayloadToServer({
         lineno: e.lineno,
         colno: e.colno,
         filename: e.filename,
         message: e.error.message,
         stack: e.error.stack
-      })
-    })
+        
+                          })
+                          })
   }
-
-}())
-
+}
 App.prototype.sendPayloadToServer = function(e) {
-  const URL= "yet to define"
+  const URL= "yet to define";
   let xhr = new XMLHttpRequest();
   xhr.open('POST', URL );
   xhr.send(e);
   xhr.onerror = function() { 
             alert(`Network Error`);
               };
-
-
-
-  // send error to server endpoint
-
 }
+
+
+App.prototype.unmount = function()
+{
+  track=false;
+}
+
+App.prototype.senderror=function()
+{
+  throw new Error()
+}
+
+
+
+var app1 =new App();
+
+// for now this is the just the error created for testing
+print(asdh);
